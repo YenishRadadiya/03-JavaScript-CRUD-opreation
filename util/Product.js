b9049f2f411a571ed024331bbe37aaa48c01b0b6
Yenish Radadiya <yenish.radadiya@simformsolutions.com>
2025-02-10 17:21:14 +0530

    UI part of CRUD operation

---
+++ util/Product.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor() {
+        prod_id = this.prod_id;
+        prod_name = this.prod_name;
+        prod_price = this.prod_price;
+        prod_img = this.prod_img;
+        prod_desc = this.prod_desc;
+    }
+
+
+}
