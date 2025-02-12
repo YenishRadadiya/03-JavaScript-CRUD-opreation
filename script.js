449db9d6be25dc7ea0be81fa02b80065f717e899
Yenish Radadiya <yenish.radadiya@simformsolutions.com>
2025-02-12 11:51:15 +0530

    Implemented sort and filter feature

--- script.js
+++ script.js
@@ -1,47 +1,103 @@
 import { base64ToFile } from "./util/handleImage.js";
-const table = document.getElementById('product_table');
-var prod_list = JSON.parse(localStorage.getItem('crud_product'));
-const no_product = document.getElementById('no-product');
+
+const table = document.getElementById("product_table");
+const sortOptions = document.getElementById("sortOptions");
+var prod_list = JSON.parse(localStorage.getItem("crud_product")) || [];
+const no_product = document.getElementById("no-product");
+
+// Function to append a single product row
+export function appendProductRow(product) {
+    let tr = document.createElement("tr");
+    let url = base64ToFile(product.prod_img, "image/jpeg");
+
+    tr.innerHTML = `
+        <td style="width: 2%">${product.prod_id}</td>
+        <td style="width: 30%"><img src='${url}' alt="Product" /></td>
+        <td style="width: 15%">${product.prod_name}</td>
+        <td>${product.prod_desc}</td>
+        <td style="width: 5%">${product.prod_price}</td>
+        <td style="width: 10%">
+            <button class="btn_view" onclick="window.location.href='./Pages/Product.html?id=${product.prod_id}'">View <i class="fa fa-eye"></i></button>
+            <button class="btn-edit" onclick="window.location.href='./Pages/AddProduct.html?mode=edit&id=${product.prod_id}'">Edit <i class="fa fa-edit"></i></button>
+            <button class="btn-dlt" onclick="handleOperation(${product.prod_id})">Delete <i class="fa fa-trash-o"></i></button>
+        </td>`;
+
+    table.appendChild(tr);
+}
+
+// Function to load products into the table
+export function loadProducts() {
+    table.innerHTML = `
+        <tr>
+            <th>No.</th>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th></th>
+        </tr>`;
+
+    if (prod_list.length === 0) {
+        no_product.style.display = "block";
+    } else {
+        no_product.style.display = "none";
+        prod_list.forEach(product => appendProductRow(product));
+    }
+}
+
+// Clear all function
 export function handleClearAll() {
     localStorage.clear();
     window.location.reload();
 }
+
+// Delete Product function
 export function handleOperation(id) {
-    for (let i = 0; i < prod_list.length; i++) {
-        let obj = prod_list[i];
-        if (id = obj.prod_id) {
-            prod_list.splice(i, 1);
+    prod_list = prod_list.filter(product => product.prod_id !== id);
+    localStorage.setItem("crud_product", JSON.stringify(prod_list));
+    loadProducts();
+}
+
+// Search function
+export function searchProduct() {
+    let id = parseInt(document.getElementById("search_id").value);
+    let filteredList = prod_list.filter(product => product.prod_id === id);
+
+    table.innerHTML = `
+        <tr>
+            <th>No.</th>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th></th>
+        </tr>`;
+
+    if (filteredList.length > 0) {
+        filteredList.forEach(product => appendProductRow(product));
+    } else {
+        alert('Product not found')
+    }
+}
+
+// Sorting function
+export function sortProducts() {
+    let criteria = sortOptions.value;
+
+    prod_list.sort((a, b) => {
+        if (criteria === "id") {
+            return a.prod_id - b.prod_id;
+        } else if (criteria === "name") {
+            return a.prod_name.localeCompare(b.prod_name);
+        } else if (criteria === "price") {
+            return a.prod_price - b.prod_price;
         }
-    }
-    localStorage.setItem('crud_product', JSON.stringify(prod_list));
-    window.location.reload();
+    });
 
-
+    localStorage.setItem("crud_product", JSON.stringify(prod_list));
+    loadProducts();
 }
 
-if (!prod_list) {
-    no_product.style.display = 'block';
-}
-else {
-    no_product.style.visibility = 'none'
-    for (let i = 0; i < prod_list.length; i++) {
-        let obj = prod_list[i];
-        let tr = document.createElement('tr');
-        let url = base64ToFile(obj.prod_img, 'image/jpeg');
-        tr.innerHTML = `<td style="width: 2%" name="prod_id" id="">${obj.prod_id}</td>
-                                <td style="width: 30%" name="prod_image">
-                                    <img src='${url}' alt="Product" />
-                                </td>
-                                <td style="width: 15%" name="prod_name" id="prod_name">${obj.prod_name}</td>
-                                <td name="prod_desc">${obj.prod_desc}</td>
-                                <td style="width: 5%" name="prod_price">${obj.prod_price}</td>
-                                <td style="width: 10%">
-                                    <button id="${obj.prod_id}" class="btn_view" onclick="window.location.href='./Pages/Product.html?id=${obj.prod_id}'" >View<i class="fa fa-eye" ></i></button>
-                                    <button id="${obj.prod_id}" class="btn-edit" onclick="window.location.href='./Pages/AddProduct.html?mode=edit&id=${obj.prod_id}'" >Edit<i class="fa fa-edit" ></i></button>
-                                    <button id="${obj.prod_id}" class="btn-dlt" onclick='handleOperation(${obj.prod_id})'>Delete<i class="fa fa-trash-o"></i></button>
-                                </td>`;
-        table.appendChild(tr);
-    }
+document.getElementById("sortButton").addEventListener("click", sortProducts);
 
-}
-
+loadProducts();
